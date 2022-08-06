56e3d2d94646a499ca972da3632e3617073c4034
David Schneider <dsbrng25b@gmail.com>
2022-08-06 18:07:07 +0200

    Implement RequestRunner, macro and traits

--- src/k8s.rs
+++ src/k8s.rs
@@ -1,4 +1,4 @@
-use k8s_openapi::api::{authentication::v1::{TokenReview, UserInfo, TokenReviewStatus, TokenReviewSpec}, authorization::v1::SubjectAccessReview};
+use k8s_openapi::api::{authentication::v1::TokenReview, authorization::v1::SubjectAccessReview};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::{
     error::Error,
@@ -6,39 +6,43 @@ use std::{
 };
 
 #[macro_export]
-macro_rules! hook_auth {
-    ($e:expr) => {
+macro_rules! register_authenticator {
+    ($e:ident) => {
         #[no_mangle]
         fn auth() {
-            //let a: $crate::k8s::Authenticator<S> = $e;
-            $crate::k8s::Reviewer::new($e).run_with_stdin().unwrap();
+            //let auth: $crate::k8s::Authenticator<S>> = $e;
+            //let auth: dyn $crate::k8s::Authenticator<S> = $e;
+            $e::runner().run_with_stdin().unwrap();
         }
     };
 }
 
-pub struct Reviewer<I, O> {
-    review: fn (input: I) -> Result<O, Box<dyn Error>>
+
+pub struct RequestRunner<I, O, S> {
+    run_fn: fn(input: I, settings: S) -> Result<O, Box<dyn Error>>,
 }
 
-impl<I, O> Reviewer<I, O> {
-    pub fn new(func: fn(I) -> Result<O, Box<dyn Error>>) -> Self {
+impl<I, O, S> RequestRunner<I, O, S>
+where
+    I: DeserializeOwned,
+    S: DeserializeOwned,
+    O: Serialize
+{
+
+    pub fn new(f: fn(input: I, settings: S) -> Result<O, Box<dyn Error>>) -> Self {
         Self {
-            review: func,
+            run_fn: f,
         }
     }
-    pub fn raw_run(&self, input: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> 
-        where I: DeserializeOwned,
-              O: Serialize
+    pub fn raw_run(&self, input: &[u8]) -> Result<Vec<u8>, Box<dyn Error>>
     {
-        let input = serde_json::from_slice(input)?;
-        let resp: Response<O> = (self.review)(input).into();
+        let req: Request<I, S> = serde_json::from_slice(input)?;
+        let resp: Response<O> = (self.run_fn)(req.request, req.settings).into();
         let output = serde_json::to_vec(&resp)?;
         Ok(output)
     }
 
     pub fn run_with_stdin(&self) -> Result<(), Box<dyn Error>>
-        where I: DeserializeOwned,
-              O: Serialize
     {
         let mut input = Vec::new();
         std::io::stdin().read_to_end(&mut input)?;
@@ -51,94 +55,32 @@ impl<I, O> Reviewer<I, O> {
     }
 }
 
-pub type Authenticator<S> = fn (Request<TokenReview, S>) -> Result<TokenReview, Box<dyn Error>>;
-pub type Authorizer<S> = fn (Request<SubjectAccessReview, S>) -> Result<SubjectAccessReview, Box<dyn Error>>;
-//type Admiter<S> = fn (Request<AR, S>) -> Result<AR, Box<dyn Error>>;
 
-// trait Authenticator<S> {
-//     fn authenticate(tr: TokenReview, settings: S) -> Result<TokenReview, Box<dyn Error>>;
-// }
+ pub trait Authenticator<S> {
+     fn authenticate(tr: TokenReview, settings: S) -> Result<TokenReview, Box<dyn Error>>;
 
-//impl<S> Runner<Request<TokenReview, S>, TokenReview> for dyn Authenticator<S> where Self: Sized {
-//    fn run(input: Request<TokenReview, S>) -> Result<TokenReview, Box<dyn Error>> {
-//        Self::authenticate(input.request, input.settings)
-//    }
-//}
+     fn runner() -> RequestRunner<TokenReview, TokenReview, S> where S: DeserializeOwned {
+        RequestRunner::new(Self::authenticate)
+     }
+ }
 
+pub trait Authorizer<S> {
+    fn authorize(tr: SubjectAccessReview, settings: S) -> Result<SubjectAccessReview, Box<dyn Error>>;
 
-pub fn authenticate(req: Request<TokenReview, Option<Settings>>) -> Result<TokenReview, Box<dyn Error>> {
-    let tr = req.request;
-    let settings = req.settings;
-    let token = match tr.spec.token {
-        Some(token) => token,
-        None => {
-            return Err("missing token".into());
-        }
-    };
-    
-    let mut response = TokenReview::default();
-    let mut status = TokenReviewStatus::default();
-    
-    // get settings or use default values
-    let settings = settings.unwrap_or(Settings {
-        token: "my-test-token".to_string(),
-        uid: "1337".to_string(),
-        user: "my-user".to_string(),
-        groups: vec!["system:masters".to_string()],
-    });
-    
-    if token == settings.token {
-        status.authenticated = Some(true);
-        status.user = Some(UserInfo {
-            username: Some(settings.user),
-            uid: Some(settings.uid),
-            groups: Some(settings.groups),
-            extra: None,
-        });
-    } else {
-        status.authenticated = Some(false);
-        status.error = Some("invalid token".to_string())
-    }
+     fn runner() -> RequestRunner<SubjectAccessReview, SubjectAccessReview, S> where S: DeserializeOwned {
+        RequestRunner::new(Self::authorize)
+     }
+ }
 
-    response.status = Some(status);
+pub trait Admiter<S> {
+    fn admit(tr: (), settings: S) -> Result<(), Box<dyn Error>>;
 
-    Ok(response)
+     fn runner() -> RequestRunner<(), (), S> where S: DeserializeOwned {
+        RequestRunner::new(Self::admit)
+     }
 }
 
-#[test]
-fn test1() -> Result<(), Box<dyn Error>> {
-    let mut tr = TokenReview::default();
-    tr.spec.token = Some("my-test-token".to_string());
-    let settings = None;
-    let result = authenticate(Request::new(tr, settings))?;
-    let result = serde_json::to_string(&result)?;
-    println!("{}", result);
-    Ok(())
-}
 
-#[test]
-fn test2() -> Result<(), Box<dyn Error>> {
-    let mut tr = TokenReview::default();
-    tr.spec.token = Some("my-test-token1".to_string());
-    let settings: Option<Settings> = None;
-    let req = Request::new(tr, settings);
-
-    let input = serde_json::to_vec(&req)?;
-    let r = Reviewer{ review: authenticate };
-
-    let output = r.raw_run(&input)?;
-    let output = String::from_utf8(output)?;
-    println!("{}", output);
-    Ok(())
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct Settings {
-    token: String,
-    uid: String,
-    user: String,
-    groups: Vec<String>,
-}
 
 #[derive(Serialize, Deserialize)]
 pub struct Request<T, S> {
@@ -147,7 +89,7 @@ pub struct Request<T, S> {
 }
 
 impl<T, S> Request<T, S> {
-    fn new(request: T, settings: S) -> Self {
+    pub fn new(request: T, settings: S) -> Self {
         Self {
             request,
             settings,
@@ -175,87 +117,6 @@ impl<T, E> Into<Response<T>> for Result<T, E> where E: Display {
 
         }
     }
-}
-
-type RunFn<I, O, S> = fn(req: Request<I, S>) -> Result<O, Box<dyn Error>>;
-
-//type AuthFn<T> = fn(tr: TokenReview, T) -> Result<TokenReview, Box<dyn Error>>;
-
-
-fn my_auth(req: Request<TokenReview, Option<Settings>>) -> Result<TokenReview, Box<dyn Error>> {
-    let tr = req.request;
-    let settings = req.settings;
-    let token = match tr.spec.token {
-        Some(token) => token,
-        None => {
-            return Err("missing token".into());
-        }
-    };
-
-    let mut response = TokenReview::default();
-    let mut status = TokenReviewStatus::default();
-
-    // get settings or use default values
-    let settings = settings.unwrap_or(Settings {
-        token: "my-test-token".to_string(),
-        uid: "1337".to_string(),
-        user: "my-user".to_string(),
-        groups: vec!["system:masters".to_string()],
-    });
-
-    if token == settings.token {
-        status.authenticated = Some(true);
-        status.user = Some(UserInfo {
-            username: Some(settings.user),
-            uid: Some(settings.uid),
-            groups: Some(settings.groups),
-            extra: None,
-        });
-    } else {
-        status.authenticated = Some(false);
-        status.error = Some("invalid token".to_string())
-    }
-
-    response.status = Some(status);
-
-    Ok(response)
-}
-
-fn raw_json_runner<I, O, S>(input: &[u8], run_fn: RunFn<I, O, S>) -> Result<Vec<u8>, Box<dyn Error>>
-where
-    I: DeserializeOwned,
-    S: DeserializeOwned,
-    O: Serialize,
-{
-    let req: Request<I, S> = serde_json::from_slice(input)?;
-    let resp = match run_fn(req) {
-        Ok(tr) => Response {
-            response: Some(tr),
-            error: None,
-        },
-        Err(err) => Response {
-            response: None,
-            error: Some(err.to_string()),
-        },
-    };
-    let output = serde_json::to_vec(&resp)?;
-    Ok(output)
-}
-
-fn stdin_out_wrapper<I, O, S>(run_fn: RunFn<I, O, S>) -> Result<(), Box<dyn Error>> 
-where
-    I: DeserializeOwned,
-    O: Serialize,
-    S: DeserializeOwned
-{
-    let mut input = Vec::new();
-    std::io::stdin().read_to_end(&mut input)?;
-
-    let output = raw_json_runner(input.as_slice(), run_fn)?;
-
-    std::io::stdout().write_all(&output)?;
-    std::io::stdout().flush()?;
-    Ok(())
 }
 
 // #[no_mangle]
